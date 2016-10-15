d8bff3e3d3df113e1e1f20f1ffa05e862dcd876e
Andrey Talyzin <andrey@minisol.ru>
2016-10-15 16:50:50 +0300

    add vox api intergration

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var AppConfig = require('./config/config.json');
 
 /*init*/
+var AppHelper = require('./libs/Helper.js');
 var express = require('express');
 var pathLoader = require('path');
 var consolidate = require('consolidate');
 var swig = require('swig');
+var AppCurl = require('curlrequest');
 var Application = express();
-var SocketIO = require("socket.io").listen(AppConfig.socketio.listen);
+Application.SocketIO = require("socket.io").listen(AppConfig.socketio.listen);
+
 
 /*load app routes*/
 var AppRoutes = require('./routes/index');
@@ -32,5 +35,31 @@ Application.use(function(err, req, res, next) {
 	res.status(err.status || 500);
 	res.sendFile('error');
 });
+
+/*socket events*/
+var conferenceRooms = {};//[roomId] => {users: {},users_count : (int)}
+Application.SocketIO.on('connection', function(socket) {
+	__log('new user connected with id: ' + socket.id);
+	socket.on('auth', function(data) {
+		if(!data.user) data.user = 'User ' + (new Date()).getTime();
+		// var CurlRequestUrl = AppConfig.vox.api + 'BindUser/'
+		// 	+ '?account_name=' + AppConfig.vox.acc_name
+		// 	+ '&api_key=' + AppConfig.vox.app_key
+		// 	+ '&user_name=' + data.user
+		// 	+ '&application_name=' + AppConfig.vox.app_name;
+
+		// AppCurl.request({url : CurlRequestUrl}, function(answer) {
+		// 	console.log('answer: ',answer);
+		// });
+	});
+});
+
+function __log(message){
+	if(typeof message == "string") {
+		console.log('[ '+(new Date()).getTime()+' ] ' + message);
+	}else{
+		console.log('[ '+(new Date()).getTime()+' ] ', message);
+	}
+}
 
 module.exports = Application;
--- public/js/app.js
+++ public/js/app.js
@@ -4,47 +4,86 @@
 var appConfig = {
 	socket : {
 		listen : "http://localhost:3001/"
+	},
+	vox : {
+		app_name : 'videoconf',
+		acc_name : 'andreym'
 	}
 };
 
 /*init sockets*/
 var SocketIO = io.connect(appConfig.socket.listen);
 
+/*init*/
+
 
 var assistApplication = angular.module('assistApplication', []);
 
-assistApplication.controller('assistController', function assistController($scope, $http) {
-	console.log('app create');
+assistApplication.factory('$socket', function($rootScope) {
+	return {
+		on: function (eventName, callback) {
+			SocketIO.on(eventName, function () {  
+				var args = arguments;
+				$rootScope.$apply(function () {
+					callback.apply(SocketIO, args);
+				});
+			});
+		},
+		emit: function (eventName, data, callback) {
+			SocketIO.emit(eventName, data, function () {
+				var args = arguments;
+				$rootScope.$apply(function () {
+					if (callback) {
+						callback.apply(SocketIO, args);
+					}
+				});
+			})
+		}
+	};
 });
 
-// /*angular app*/
-// var minisolAssist = angular.module('minisolAssist');
-
-// /*custom factory*/
-// minisolAssist.factory('$socket', function ($rootScope) {
-// 	return {
-// 		on: function (eventName, callback) {
-// 			socket.on(eventName, function () {  
-// 				var args = arguments;
-// 				$rootScope.$apply(function () {
-// 					callback.apply(socket, args);
-// 				});
-// 			});
-// 		},
-// 		emit: function (eventName, data, callback) {
-// 			socket.emit(eventName, data, function () {
-// 				var args = arguments;
-// 				$rootScope.$apply(function () {
-// 					if (callback) {
-// 						callback.apply(socket, args);
-// 					}
-// 				});
-// 			})
-// 		}
-// 	};
-// });
 
 
-// minisolAssist.controller('assistController',function assistController($scope,$http){
-// 	console.log('application create');
-// });
+assistApplication.controller('assistController', function assistController($scope, $socket) {
+	$scope.voxAPI = VoxImplant.getInstance();
+	$scope.clientName = Query.clientName || 'User ' + (new Date()).getTime();
+	$scope.conferenceID = Query.conferenceID || false;
+
+	/*vox events*/
+	$scope.voxAPI.addEventListener(VoxImplant.Events.SDKReady, function(event) {
+		$scope.userConnect();
+	});
+	
+	$scope.voxAPI.addEventListener(VoxImplant.Events.ConnectionEstablished, function(){
+		console.info('connection: ',$scope.voxAPI.connected());
+	});
+	
+	$scope.voxAPI.addEventListener(VoxImplant.Events.ConnectionFailed, function(){
+
+	});
+	
+	$scope.voxAPI.addEventListener(VoxImplant.Events.ConnectionClosed, function(){
+
+	});
+	
+	$scope.voxAPI.addEventListener(VoxImplant.Events.AuthResult, function(event){
+
+	});
+	
+	$scope.voxAPI.addEventListener(VoxImplant.Events.IncomingCall, function(event){
+
+	});
+	
+	$scope.voxAPI.addEventListener(VoxImplant.Events.MicAccessResult, function(){
+
+	});
+
+	/*scope functions*/
+	$scope.userConnect = function() {
+		console.info('connect vox');
+		$scope.voxAPI.connect();
+	}
+
+	
+});
+
